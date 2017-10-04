03dc8bfa7a2e9c9c350b70d87bd403f09b9db51a
ksbains <k1bains@ucsd.edu>
2017-10-04 12:31:30 -0700

    started the spotify search and made the toQueryURL method

--- liri.js
+++ liri.js
@@ -1,33 +1,54 @@
+// Twitter Stuff
 var keys = require("./keys.js");
-//console.log(keys);
-// var twitterKeys = {
-//   consumer_key: "ANrn5JV57XppSLzl7BhIIuvEu",
-//   consumer_secret: "Kd85d9lUDrWZYfA2OAmnxK7GOmEN1rdzjcSAJbthdJk9nO2p4g",
-//   access_token_key: "786594535631572993-sE8Fg0wRNJnjsHokFGECvU1DmqNqiYN",
-//   access_token_secret: "s0inGs3zMt9Xb3OVk34zKPWnqRwptopS9ZitopHC85aJf"
-// }
 var Twitter = require('twitter');
- 
 var client = new Twitter({
   consumer_key: keys.consumer_key ,
   consumer_secret: keys.consumer_secret,
   access_token_key: keys.access_token_key,
   access_token_secret: keys.access_token_secret
 });
- 
-
+// Spotify Stuff
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+  id: "b1adb519b7d94a65ba45fab2d8e0acd0",
+  secret: "ad55a14b45164bfbac0c9da2499f5d60"
+});
+//Input Variables
 var action = process.argv[2];
 var value = process.argv[3];
+//Functions
+function readAll(arr, start){
+	var movieName = "";
+	for(var i = start; i < arr.length; i++){
+		movieName = movieName + " " + arr[i];
+	}
+	return movieName;
+}
+
+function toQueryString(value) {
+	var queryString = {
+	    t: value,
+	    type: "movie",
+	    r: "JSON"
+	};
+    var baseUrl = "http://www.omdbapi.com/?apikey=40e9cece&"
+    var qString = '';
+    for (var i in queryString) {
+        qString += '&' + i + '=' + this.queryString[i];
+    }
+    return baseUrl + qString.trim('&');
+};
 
 switch(action){
 	case "my-tweets":
 		console.log("You have selected the twitter option");
+		// This will show your last 20 tweets and when they were created at in your terminal/bash window.
 		var params = {screen_name: 'kingbainz'};
 		client.get('statuses/user_timeline', params, function(error, tweets, response){
 		  if (!error){
-		    //console.log(tweets);
+		    console.log(tweets);
 		    for(var i = 0; i <tweets.length; i++){
-		    	console.log(tweets[i].text)
+		    	//console.log(tweets[i].text)
 		    }
 		  }
 		});
@@ -36,8 +57,24 @@ switch(action){
 	case "spotify-this-song":
 		//spotify stuff
 		console.log("You have selected the spotify option");
+		value = readAll(process.argv, 3);
 		console.log("the song you have selected is: " + value);
+		spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+		  if (err) {
+		    return console.log('Error occurred: ' + err);
+		  } 
+		console.log(data); 
+			console.log(data.tracks.items[0]);
+			//.album.artist[0].name);
+		});
+
+		// need to console log 
+		// Artist(s)
+		// The song's name
+		// A preview link of the song from Spotify
+		// The album that the song is from
 		break;
+
 	case "movie-this":
 		//movie stuff
 		console.log("You have selected the movie option");
